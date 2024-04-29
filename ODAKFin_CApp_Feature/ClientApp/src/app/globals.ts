caae6a8e856cfd508c3135ab5460f428d61839c8
PanneerTwilight <Panneerselvam@twilightsoftwares.com>
2024-04-29 12:57:48 +0530

    changes on the global.ts

--- ODAKFin_CApp_Feature/ClientApp/src/app/globals.ts
+++ ODAKFin_CApp_Feature/ClientApp/src/app/globals.ts
@@ -7,9 +7,9 @@ export class Globals {
     appName: string = 'FI';
 
     // UAT
-    //APIURL: string = 'https://odakfnuat.odaksolutions.in/api';
+    APIURL: string = 'https://odakfnuat.odaksolutions.in/api';
     MASTER_API_URL: string = 'https://odakfnuat.odaksolutions.in/api';
-    //SaApi: string = "https://odaksauat.odaksolutions.in/api";
+    SaApi: string = "https://odaksauat.odaksolutions.in/api";
     APIURLFF: string = 'https://odaksauat.odaksolutions.in/ffddev/api';
     APIURLLA: string = 'https://odaklauat.odaksolutions.in/api';
     TemplateUploadURL: string = 'https://odakfnuat.odaksolutions.in';
