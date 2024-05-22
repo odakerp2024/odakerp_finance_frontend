cb627033aa539fe482a03032f9fb29d44ddce2e1
PanneerTwilight <Panneerselvam@twilightsoftwares.com>
2024-05-22 18:47:37 +0530

    CustomerMasterworkflowchanges in ts file for KYC

--- ODAKFin_CApp_Feature/ClientApp/src/app/views/finance/master/customer/customer.component.ts
+++ ODAKFin_CApp_Feature/ClientApp/src/app/views/finance/master/customer/customer.component.ts
@@ -829,10 +829,12 @@ export class CustomerComponent implements OnInit {
     }
 
     //KYC Documents
-    if (response['data'].Table4.length > 0) {
-      this.documentPayloadInfo = response['data'].Table3;
-      if(this.onBoardStatus != 2){
-        this.disableSave = false;
+    if (response['data'].Table3.length > 0) {
+      if (response['data'].Table4.length > 0) {
+        this.documentPayloadInfo = response['data'].Table3;
+        if(this.onBoardStatus != 2){
+          this.disableSave = false;
+        }
       }
     }
 
