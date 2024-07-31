cf6d968c85eb6329ca9115f3f97303e2983c3492
PanneerTwilight <Panneerselvam@twilightsoftwares.com>
2024-07-31 21:42:43 +0530

    environment file changes in global.ts

--- ODAKFin_CApp_Feature/ClientApp/src/app/globals.ts
+++ ODAKFin_CApp_Feature/ClientApp/src/app/globals.ts
@@ -16,5 +16,33 @@ export class Globals {
     TemplateUploadURL: string = 'https://odakfnuat.odaksolutions.in';
     LANDINGURL: string = 'https://odakfnuat.odaksolutions.in/api'
 
+    // QA
+    // APIURL: string = 'https://odakfnqa.odaksolutions.in/api';
+    // MASTER_API_URL: string = 'https://odakfnqa.odaksolutions.in/api';
+    // SaApi: string = "https://odaksaqa.odaksolutions.in/api";
+    // APIURLFF: string = 'https://odaksaqa.odaksolutions.in/ffddev/api';
+    // APIURLLA: string = 'https://odaklaqa.odaksolutions.in/api';
+    // TemplateUploadURL: string = 'https://odakfnqa.odaksolutions.in';
+    // LANDINGURL: string = 'https://sa-navioindia.freighteiz.com/api'
+
+    // dev
+    // APIURL: string = 'https://odakfn.odaksolutions.in/api';
+    // // APIURL: string = 'https://localhost:44301/api';
+    // MASTER_API_URL: string = 'https://odakfn.odaksolutions.in/api';
+    // SaApi: string = "https://odaksaqa.odaksolutions.in/api";
+    // APIURLFF: string = 'https://odaksaqa.odaksolutions.in/ffddev/api';
+    // APIURLLA: string = 'https://odaklaqa.odaksolutions.in/api';
+    // TemplateUploadURL: string = 'https://odakfn.odaksolutions.in';
+    // LANDINGURL: string = 'https://navioindia.freighteiz.com'
+
+    // PROD
+    // APIURL: string = 'https://fn-navioindia.freighteiz.com/api';
+    // MASTER_API_URL: string = 'https://fn-navioindia.freighteiz.com/api';
+    // SaApi: string = "https://sa-navioindia.freighteiz.com/api";
+    // APIURLFF: string = 'https://sa-navioindia.freighteiz.com/ffddev/api';
+    // FFAPI: string = 'https://ff-navioindia.freighteiz.com/ffddev/api';
+    // APIURLLA: string = 'https://la-navioindia.freighteiz.com/api';
+    // TemplateUploadURL: string = 'https://fn-navioindia.freighteiz.com';
+    // LANDINGURL: string = 'https://navioindia.freighteiz.com/'
 
 }
